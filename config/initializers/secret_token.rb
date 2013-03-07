a0e8b32accbd49908365966ca297c40c25e106c1
ben <ben@ubuntuBen.dynamic.kbronet.com.tw>
2013-03-07 20:44:01 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MovieAnalytics::Application.config.secret_token = 'e334bec8a614f332fc38ed09cbd1dbbc812d70b62dc8f842cf08d4c96549dfee973c885959c38e796b022f50a74256f416a93f2e2ee5c3c03374e7522d91deb1'
